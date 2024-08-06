b7be3089bbfab893467e2647bdde8414c2996c17
necroplod <84621214+necroplod@users.noreply.github.com>
2024-08-06 23:00:25 +0300

    pensil

--- config/config.py
+++ config/config.py
@@ -1,6 +1,6 @@
-TOKEN = '7089805789:AAGtDKaVRVvYGnMF2EpZCLGaGLF9_1qI0jA'
+TOKEN = ''
 
-MONGOURI = 'mongodb+srv://daddy:penis111111@db.dciilv7.mongodb.net/?retryWrites=true&w=majority&appName=db'
-DBNAME = 'pensil'
+MONGOURI = ''
+DBNAME = ''
 
 YOOMONEY = ''
