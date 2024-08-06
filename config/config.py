7587bedb56bf05cafd449967f2c69711a8634cbd
necro <necroplod@mail.ru>
2024-08-06 22:56:54 +0300

    added config

---
+++ config/config.py
@@ -0,0 +1,6 @@
+TOKEN = '7089805789:AAGtDKaVRVvYGnMF2EpZCLGaGLF9_1qI0jA'
+
+MONGOURI = 'mongodb+srv://daddy:penis111111@db.dciilv7.mongodb.net/?retryWrites=true&w=majority&appName=db'
+DBNAME = 'pensil'
+
+YOOMONEY = ''
